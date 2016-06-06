78800b4f9f9da49d69a920ef1860f3b679b7590f
Juan Pascale <juan.pascale@sabf.org.ar>
2016-06-06 03:17:41 -0300

    Add slf4j logger

--- metrics/src/main/java/ar/edu/itba/protos/pstat/main/Main.java
+++ metrics/src/main/java/ar/edu/itba/protos/pstat/main/Main.java
@@ -1,8 +1,24 @@
 package ar.edu.itba.protos.pstat.main;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class.getSimpleName());
+
     public static void main (String[] args){
-        System.out.println("Hola!");
+        if (args.length != 1){
+            System.out.println("Invalid amount of arguments. Path to the config file is needed.");
+            LOG.error("Invalid amount of arguments");
+            System.exit(1);
+        }
+
+        LOG.info("Right amount of arguments");
+
+        
+
+
+
     }
 }
